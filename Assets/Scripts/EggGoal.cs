fe9c3030736758b95f60c5766e0ee0ba032b9bee
ricenh <ricenh@dukes.jmu.edu>
2025-04-06 21:21:29 -0400

    completed game

--- Assets/Scripts/EggGoal.cs
+++ Assets/Scripts/EggGoal.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class EggGoal : MonoBehaviour
 {
     private int eggCount = 0;
-    public int targetEggs = 3;
+    public int targetEggs = 1;
+
+    public GameObject winCanvas; 
+
+    private void Start()
+    {
+        if (winCanvas != null)
+        {
+            winCanvas.SetActive(false); 
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Egg"))
         {
             eggCount++;
+
             if (eggCount >= targetEggs)
             {
                 Debug.Log("You won!");
+                if (winCanvas != null)
+                {
+                    winCanvas.SetActive(true); 
+                }
             }
         }
     }
