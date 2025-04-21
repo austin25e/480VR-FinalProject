90ef7c3a1ef5c708fa8d1c1801bafc9e1318a14d
Sayemum <sayemumhassan@gmail.com>
2025-04-21 10:38:38 -0400

    Minor TM and NPC improvements

--- Assets/Scripts/wheatCounter.cs
+++ Assets/Scripts/wheatCounter.cs
@@ -8,12 +8,14 @@ public class wheatCounter : MonoBehaviour
     public XRSocketInteractor socket1;
     public XRSocketInteractor socket2;
     public XRSocketInteractor socket3;
+    public TMButtonScript nextButton;
 
     private void OnTriggerEnter(Collider other)
     {
         if (socket1.hasSelection && socket2.hasSelection && socket3.hasSelection)
         {
             winText.SetActive(true);
+            nextButton.isUnlocked = true;
             Invoke("HideWinText", 8f);
         }
     }
