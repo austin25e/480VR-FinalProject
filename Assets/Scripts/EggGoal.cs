36d7c13557e6282dc6f3fb5f463ff96c7d7748b5
ricenh <ricenh@dukes.jmu.edu>
2025-04-28 15:05:09 -0400

    egg changes

--- Assets/Scripts/EggGoal.cs
+++ Assets/Scripts/EggGoal.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class EggGoal : MonoBehaviour
 {
     private int eggCount = 0;
     public int targetEggs;
 
-    public GameObject winCanvas; 
+    public GameObject winCanvas;
     public TMButtonScript nextButton;
+
+    private HashSet<GameObject> countedEggs = new HashSet<GameObject>();
 
     private void Start()
     {
         if (winCanvas != null)
         {
-            winCanvas.SetActive(false); 
+            winCanvas.SetActive(false);
         }
     }
 
@@ -21,16 +24,42 @@ public class EggGoal : MonoBehaviour
     {
         if (other.CompareTag("Egg"))
         {
-            eggCount++;
-
-            if (eggCount >= targetEggs)
+            if (!countedEggs.Contains(other.gameObject))
             {
-                Debug.Log("You won!");
+                countedEggs.Add(other.gameObject);
+                eggCount++;
+                CheckWin();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Egg"))
+        {
+            if (countedEggs.Contains(other.gameObject))
+            {
+                countedEggs.Remove(other.gameObject);
+                eggCount--;
+
+                // Optional: hide the win canvas if an egg leaves
                 if (winCanvas != null)
                 {
-                    winCanvas.SetActive(true); 
-                    nextButton.UnlockButton(true);
+                    winCanvas.SetActive(false);
                 }
+            }
+        }
+    }
+
+    private void CheckWin()
+    {
+        if (eggCount >= targetEggs)
+        {
+            Debug.Log("You won!");
+            if (winCanvas != null)
+            {
+                winCanvas.SetActive(true);
+                nextButton.UnlockButton(true);
             }
         }
     }
